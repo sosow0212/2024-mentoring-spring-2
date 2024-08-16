25a86404cd10dde1abe63d47a4116ffba73fafd3
승엽 <tmdduqflfl@naver.com>
2024-08-16 17:39:35 +0900

    Refactor: 코드리뷰반영

--- mission-lotto/src/test/java/com/web/user/domain/entity/UserTest.java
+++ mission-lotto/src/test/java/com/web/user/domain/entity/UserTest.java
@@ -7,7 +7,7 @@ public class UserTest {
 
     @Test
     public void 돈입력_테스트() {
-        User user = new User(1L, "sy", 10000, 0,0,null);
+        User user = new User(1L, "sy", 10000, 0,null);
 
 
         assertEquals(10000, user.getBalance());
