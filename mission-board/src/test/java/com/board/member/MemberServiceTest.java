5f1593420ffc89040cf317556d60a981ed62985d
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-09-10 17:08:19 +0900

    Feat: 테스트 코드 작성

--- mission-board/src/test/java/com/board/member/MemberServiceTest.java
+++ mission-board/src/test/java/com/board/member/MemberServiceTest.java
@@ -2,6 +2,8 @@ package com.board.member;
 
 import com.board.member.repository.MemberRepository;
 import com.board.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -15,4 +17,40 @@ public class MemberServiceTest {
 
     @InjectMocks
     private MemberService memberService;
+
+    @Test
+    @DisplayName("중복되지 않는 새로운 유저를 생성한다.")
+    void signUp_newMember() {
+
+    }
+
+    @Test
+    @DisplayName("닉네임이 중복되는 새로운 유저를 생성한다.")
+    void signUp_duplicate_nickName_member() {
+
+    }
+
+    @Test
+    @DisplayName("아이디가 중복되는 새로운 유저를 생성한다.")
+    void signUp_duplicate_loginId_member() {
+
+    }
+
+    @Test
+    @DisplayName("유저 정보를 업데이트 한다.")
+    void update_member() {
+
+    }
+
+    @Test
+    @DisplayName("유저 정보를 조회한다.")
+    void find_member() {
+
+    }
+
+    @Test
+    @DisplayName("유저 정보를 삭제한다.")
+    void delete_member() {
+
+    }
 }
