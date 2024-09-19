069e995d9f47d519fb86c7350613aae46dfdae64
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-09-19 15:28:25 +0900

    Feat: ArticleTest 테스트 코드 작성

--- mission-board/src/test/java/com/board/article/domain/ArticleTest.java
+++ mission-board/src/test/java/com/board/article/domain/ArticleTest.java
@@ -1,4 +1,35 @@
 package com.board.article.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class ArticleTest {
+
+    private Article article;
+
+    @BeforeEach
+    void setUp() {
+        article = new Article(1L, "aa", "bb");
+    }
+
+    @DisplayName("게시글 업데이트 테스트.")
+    @Test
+    void update_article() {
+        //given
+        String updateTitle = "a";
+        String updateContent = "b";
+
+        //when
+        article.updateArticle(updateTitle, updateContent);
+
+        //then
+        assertEquals(updateTitle, article.getTitle());
+        assertEquals(updateContent, article.getContent());
+    }
 }
